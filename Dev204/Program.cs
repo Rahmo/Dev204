51956c4b601e6c49aaf3a25c7cb71b34015a844f
Rahmo <Rahmo.alzahrani@yahoo.com>
2016-02-28 12:36:09 -0600

    Mod5

--- Dev204/Program.cs
+++ Dev204/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +13,6 @@ namespace Dev204
         {
 
             #region Mod1
-
 
             //Mod1
             string FirstName = "Rahmo";
@@ -77,25 +77,63 @@ namespace Dev204
 
             #endregion
 
-            #region Mod4
-            // The index starts from 0 
-            Student[] structStudents = new Student[4];
-            structStudents[0].FirstName = "Rahmo";
-            structStudents[0].Country = "USA";
-            structStudents[0].LastName = "Alzahrani";
-            structStudents[2].FirstName = "John";
-            structStudents[2].Country = "UK";
+            //#region Mod4
+            //// The index starts from 0 
+            //Student[] structStudents = new Student[4];
+            //structStudents[0].FirstName = "Rahmo";
+            //structStudents[0].Country = "USA";
+            //structStudents[0].LastName = "Alzahrani";
+            //structStudents[2].FirstName = "John";
+            //structStudents[2].Country = "UK";
 
-            Console.WriteLine("The First assigned student information : \n First Name " + structStudents[0].FirstName 
-                + "\n Last Name : "+ structStudents[0].LastName + " \n The Student country is from : " + structStudents[0].Country +
-                "\n ****** \n " +
-                "The Second assigned student information : \n First Name " + structStudents[2].FirstName
-                + " \n The Student country is from : " + structStudents[2].Country
-                );
+            //Console.WriteLine("The First assigned student information : \n First Name " + structStudents[0].FirstName
+            //    + "\n Last Name : " + structStudents[0].LastName + " \n The Student country is from : " + structStudents[0].Country +
+            //    "\n ****** \n " +
+            //    "The Second assigned student information : \n First Name " + structStudents[2].FirstName
+            //    + " \n The Student country is from : " + structStudents[2].Country
+            //    );
 
+            //#endregion
+
+            #region Mod5
+            Student Student1 = new Student();
+            Student1.FirstName = "Rahmo";
+            Student1.LastName = "Alzahrani ";
+            Student1.AddressLine = "CHi"; 
+            Student Student2 = new Student();
+            Student2.FirstName = "John";
+            Student2.LastName = "Alzahrani ";
+            Student2.AddressLine = "CHi";
+
+            Student Student3 = new Student();
+            Student3.FirstName = "Samy";
+            Student3.LastName = "Alzahrani ";
+            Student3.AddressLine = "CHi";
+            Teacher Teacher1 = new Teacher();
+            Teacher1.FirstName = "Todd"; 
+            Course course1 = new Course();
+            course1.CourseName = "Programming with C#";
+            course1.Credits = 4;
+            course1._students = new Student[3];
+            course1._students[0] = Student1;
+            course1._students[1] = Student2;
+            course1._students[2] = Student3;
+            course1.teacher = Teacher1; 
+
+            Degree degree1 = new Degree();
+            degree1.DegreeName = " The Bachelor of Science degree ";
+            degree1.course = course1;
+  
+            Uprogram program1 = new Uprogram();
+            program1.ProgramName = " Information Technology";
+            program1.Degree = degree1; 
+
+
+
+            Console.WriteLine(" The  " + program1.ProgramName + " Program contains" + program1.Degree.DegreeName +" \n " +
+                              " "+ program1.Degree.DegreeName + " Contains the course " + program1.Degree.course.CourseName +" \n" 
+                              +" the " + program1.Degree.course.CourseName +" course contains " + Student.NoOfStudentsEnrolled +" Students Enrolled !");
             #endregion
-
-
 
             Console.ReadLine(); // This line to keep the consol running to veiw the output.
 
@@ -103,69 +141,71 @@ namespace Dev204
 
         }
 
+        //#region HelpingMethodsMod4
+        //public struct Student
+        //{
+        //    // This is the student constructor.
+        //    public Student(string firstName, string lastName, string birthdate, string country)
+        //    {
+        //        this.FirstName = firstName;
+        //        this.LastName = lastName;
+        //        this.Birthdate = birthdate;
+        //        this.Country = country;
+        //    }
+        //    // These statements declare the struct fields
+        //    public string FirstName;
+        //    public string LastName;
+        //    public string Birthdate;
+        //    public string Country;
+        //}
+        //public struct Teacher
+        //{
+        //    // This is the student constructor.
+        //    public Teacher(string firstName, string lastName, string country)
+        //    {
+        //        this.FirstName = firstName;
+        //        this.LastName = lastName;
+        //        this.Country = country;
+        //    }
+        //    // These statements declare the struct fields
+        //    public string FirstName;
+        //    public string LastName;
+        //    public string Country;
+        //}
+        //public struct program
+        //{
+        //    // This is the student constructor.
+        //    public program(string programName, string departmentHead, string degrees)
+        //    {
+        //        this.ProgramName = programName;
+        //        this.DepartmentHead = departmentHead;
+        //        this.Degrees = degrees;
 
-        public struct Student
-        {
-            // This is the student constructor.
-            public Student(string firstName, string lastName, string birthdate, string country)
-            {
-                this.FirstName = firstName;
-                this.LastName = lastName;
-                this.Birthdate = birthdate;
-                this.Country = country;
-            }
-            // These statements declare the struct fields
-            public string FirstName;
-            public string LastName;
-            public string Birthdate;
-            public string Country;
-        }
-        public struct Teacher
-        {
-            // This is the student constructor.
-            public Teacher(string firstName, string lastName, string country)
-            {
-                this.FirstName = firstName;
-                this.LastName = lastName;
-                this.Country = country;
-            }
-            // These statements declare the struct fields
-            public string FirstName;
-            public string LastName;
-            public string Country;
-        }
-        public struct program
-        {
-            // This is the student constructor.
-            public program( string programName, string departmentHead, string degrees)
-            {
-                this.ProgramName = programName;
-                this.DepartmentHead = departmentHead;
-                this.Degrees = degrees;
-             
-            }
-            // These statements declare the struct fields
-            public string ProgramName;
-            public string DepartmentHead;
-            public string Degrees;
-        
-        }
-        public struct course
-        {
-            // This is the student constructor.
-            public course(string courseName, string courseTeacher, string creditsrequired)
-            {
-                this.CourseName = courseName;
-                this.CourseTeacher = courseTeacher;
-                this.Creditsrequired = creditsrequired;
-               
-            }
-            // These statements declare the struct fields
-            public string CourseName;
-            public string CourseTeacher;
-            public string Creditsrequired;
-    
-        }
+        //    }
+        //    // These statements declare the struct fields
+        //    public string ProgramName;
+        //    public string DepartmentHead;
+        //    public string Degrees;
+
+        //}
+        //public struct course
+        //{
+        //    // This is the student constructor.
+        //    public course(string courseName, string courseTeacher, string creditsrequired)
+        //    {
+        //        this.CourseName = courseName;
+        //        this.CourseTeacher = courseTeacher;
+        //        this.Creditsrequired = creditsrequired;
+
+        //    }
+        //    // These statements declare the struct fields
+        //    public string CourseName;
+        //    public string CourseTeacher;
+        //    public string Creditsrequired;
+
+        //}
+        //#endregion
+
         #region Mod3HelpingMethods
 
 
@@ -267,5 +307,52 @@ namespace Dev204
             Console.WriteLine("THe course {0} has the teacher {1} with credits of: {2}", name, courseTeacher, CreditsReq);
         }
         #endregion
-    }
+
+        #region HelpingMethodsMod5
+
+      public class Student
+      {
+          public static int NoOfStudentsEnrolled;
+            
+          public Student()
+          {
+              NoOfStudentsEnrolled++;
+          }
+
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string AddressLine { get; set; }
+        }
+        public class Teacher
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string AddressLine { get; set; }
+        }
+
+        public class Course 
+        {
+            public string CourseName { get; set; }
+            public int Credits { get; set; }
+            public Teacher teacher { get; set; }
+            public Student[] _students { get; set; }
+        }
+
+        public class Degree
+        {
+            public string DegreeName { get; set; }
+            public Course course { get; set; }
+
+        }
+
+        public class Uprogram
+        {
+            public string ProgramName { get; set; }
+            public Degree Degree { get; set; }
+        }
+            #endregion
+
+
+
+        }
 }
